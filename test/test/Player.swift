eec0cde8d8ff343e8e5c96b83e76799549864296
Artem Chernousov <artem.chernousov@axonista.com>
2022-02-10 22:19:17 +0300

    remove redundant empty funcs

--- test/test/Player.swift
+++ test/test/Player.swift
@@ -9,17 +9,4 @@ import AVFoundation
 
 class Player: AVQueuePlayer {
     var isStarted: Bool = false
-
-    override func seek(to time: CMTime) {
-        print("<<<DEV>>> seek(to time: CMTime)")
-
-        print("<<<DEV>>> time \(time)")
-    }
-
-    override func seek(to time: CMTime, toleranceBefore: CMTime, toleranceAfter: CMTime, completionHandler: @escaping (Bool) -> Void) {
-
-        print("<<<DEV>>> seek(to time: CMTime, toleranceBefore: CMTime, toleranceAfter: CMTime, completionHandler: @escaping (Bool) -> Void)")
-
-        print("<<<DEV>>> time \(time)")
-    }
 }
